975d27f915b4ee13825132831cc804aa0d737b0c
DevByteR <mdromman417@gmail.com>
2026-05-05 21:20:11 +0600

    Navbar Section added

--- src/components/shared/NavBar.jsx
+++ src/components/shared/NavBar.jsx
@@ -1,9 +1,32 @@
-import React from 'react';
+import Image from 'next/image';
+import Link from 'next/link';
+import user from '@/assets/user.png';
 
 const NavBar = () => {
     return (
-        <div>
-            <h2>This is navbar</h2>
+        <div className='flex  items-center justify-between mt-7'>
+            <div>
+
+            </div>
+           
+            <div>
+                <ul className='flex justify-center text-center gap-3 items-center mx-auto lg:ml-40'>
+                    <li>
+                        <Link href="/">Home</Link>
+                    </li>
+                    <li>
+                        <Link href='#'>About</Link>
+                    </li>
+                    <li>
+                        <Link href='#'>Career</Link>
+                    </li>
+                </ul>
+            </div>
+            <div className='flex items-center justify-center  gap-3'>
+                <Image src={user} alt='User'/>
+                <button className='px-10 py-4 bg-black/50 text-white cursor-pointer font-semibold'>Login</button>
+
+            </div>
             
         </div>
     );
